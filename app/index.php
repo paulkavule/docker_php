fde4d9114563376832b8c800bc9d5f4f83b947bf
paul <pkavule@gmail.com>
2023-02-28 15:42:34 +0300

    setting up php application in docker

---
+++ app/index.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo '<p>Hello world</p>';
+    
+        echo '<br/>The driver is: ' .$_ENV["DRIVER"] . '!';?>
+
+</body>
+</html>
